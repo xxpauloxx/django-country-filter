898ba11c2a5672a704edb99d94663a5e703fdfa8
Paulo Roberto <paulo.pinda@gmail.com>
2020-06-19 19:43:15 -0300

    Fix scripts.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
+OMIT=".env,**/test*"
+
 # Clear old files.
-if test -f "cobertura.xml"; then
-    rm cobertura.xml
-fi
-if test -f "coverage.xml"; then
-    rm coverage.xml
-fi
-if test -f ".pytest_cache"; then
-    rm -rf .pytest_cache
-fi
+rm cobertura.xml
+rm coverage.xml
+rm -rf .pytest_cache
 
 # Generate report to coverage.
 coverage run -m pytest
 coverage xml --omit=".env,*/test*"
+
 mv coverage.xml cobertura.xml
 coverage report

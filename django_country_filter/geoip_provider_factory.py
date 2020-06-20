7de9ad5e3932ea3685a92975f57e56c7070596b0
Paulo Roberto <paulo.pinda@gmail.com>
2020-06-20 13:52:35 -0300

    Fix static method geoip provider factory.

--- django_country_filter/geoip_provider_factory.py
+++ django_country_filter/geoip_provider_factory.py
@@ -11,7 +11,8 @@ from django_country_filter.providers.geoip.default_geoip_provider import (
 class GeoipProviderFactory:
     """Class factory for geoip providers."""
 
-    def get_custom_provider(self, request):
+    @staticmethod
+    def get_custom_provider(request):
         """Return a provider according to the provider creation conventions."""
         configuration = settings.DJANGO_COUNTRY_FILTER
         package = import_module('{}'.format(
@@ -22,7 +23,7 @@ class GeoipProviderFactory:
     def __init__(self, request):
         """Class initializer."""
         try:
-            self.provider = self.get_custom_provider(request)
+            self.provider = GeoipProviderFactory.get_custom_provider(request)
         except Exception:
             self.provider = DefaultGeoipProvider(request)
 

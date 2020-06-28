9aaec38167202c2227cdec59482541656ea9a683
Paulo Roberto <paulo.pinda@gmail.com>
2020-06-28 16:11:06 -0300

    Refactor middleware.

--- django_country_filter/__init__.py
+++ django_country_filter/__init__.py
@@ -16,7 +16,11 @@ log = logging.getLogger(__name__)
 class DjangoCountryFilterMiddleware:
     """Django middleware to call the classes factories or default providers."""
 
-    configuration = None
+    __TAG = 'DjangoCountryFilterMiddleware'
+
+    __CONFIGURATION = None
+    __CACHE_PROVIDER = None
+    __GEOIP_PROVIDER = None
 
     def __init__(self, get_response):
         """Middleware initializer."""
@@ -26,54 +30,56 @@ class DjangoCountryFilterMiddleware:
         """Check countries configuration."""
         countries = self.configuration.get('countries')
         if not isinstance(countries, list):
-            (DjangoCountryFilterMiddleware
-                .logging_check_countries_in_settings())
+            self.logging_check_countries_in_settings()
             return False
         return True
+
+    def is_blocked_from_cache(self, request):
+        """Check ip on cache and block if not exist countries settings."""
+        ip = request.META.get('REMOTE_ADDR')
+        self.__CACHE_PROVIDER = CacheProviderFactory(request)
+        country = self.__CACHE_PROVIDER.get().get('country')
+        if country is None:
+            return {'cache': 'NONE'}
+        if self.country_is_blocked(country) and country is not None:
+            self.logging_country_is_blocked(country, ip)
+            return {'cache': 'BLOCK'}
+        return {'cache': 'RELEASE'}
+
+    def is_blocked_from_geoip(self, request):
+        """Check ip and block if country not exist countries settings."""
+        ip = request.META.get('REMOTE_ADDR')
+        response = GeoipProviderFactory(request).get()
+        country = response.get('country')
+        self.__CACHE_PROVIDER.provider.persist(response, datetime.now())
+        if self.country_is_blocked(country):
+            self.logging_country_is_blocked(country, ip)
+            return True
+        return False
 
     def __call__(self, request: HttpRequest):
         """Check the middleware settings and calls the provider."""
         if hasattr(settings, 'DJANGO_COUNTRY_FILTER'):
             self.configuration = settings.DJANGO_COUNTRY_FILTER
-            request_ip = request.META.get('REMOTE_ADDR')
-
             if self.check_countries_in_settings():
-                cache_provider = CacheProviderFactory(request)
-                cache_country = cache_provider.get().get('country')
-
-                if (self.country_is_blocked(cache_country)
-                        and cache_country is not None):
-                    (DjangoCountryFilterMiddleware
-                        .logging_country_is_blocked(cache_country, request_ip))
+                cache_result = self.is_blocked_from_cache(request)
+                if cache_result.get('cache') == 'BLOCK':
                     return HttpResponseForbidden()
-
-                geoip_response = GeoipProviderFactory(request).get()
-                geoip_country = geoip_response.get('country')
-                cache_provider.provider.persist(geoip_response, datetime.now())
-
-                if self.country_is_blocked(geoip_country):
-                    (DjangoCountryFilterMiddleware
-                        .logging_country_is_blocked(geoip_country, request_ip))
-                    return HttpResponseForbidden()
-
+                elif cache_result.get('cache') == 'RELEASE':
+                    return self.get_response(request)
+                elif cache_result.get('cache') == 'NONE':
+                    if self.is_blocked_from_geoip(request):
+                        return HttpResponseForbidden()
         return self.get_response(request)
 
     def country_is_blocked(self, country):
         """Check if the country is blocked."""
         return country not in self.configuration.get('countries')
 
-    @staticmethod
-    def logging_country_is_blocked(country, ip):
+    def logging_country_is_blocked(self, country, ip):
         """Log information of the block request."""
-        log.warning("""
-            DJANGO_COUNTRY_FILTER:
-            {} is blocked with ip address {}. 
-        """.format(country, ip))
+        log.warning(f'{self.__TAG}: {country} is blocked with ip address {ip}.')
 
-    @staticmethod
-    def logging_check_countries_in_settings():
+    def logging_check_countries_in_settings(self):
         """Log information when countries is not a list or not configured."""
-        log.warning("""
-            DJANGO_COUNTRY_FILTER:
-            countries in settings is not a list or not exist.
-        """)
+        log.warning(f'{self.__TAG}: Countries is not a list or not exist.')
